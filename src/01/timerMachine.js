61237ee1ac63e640f1ea60d5ef5d52dbacfa62c0
Fenix4088 <fenix4088@gmail.com>
2022-10-23 12:10:03 +0300

    exr 01

--- src/01/timerMachine.js
+++ src/01/timerMachine.js
@@ -2,3 +2,33 @@ import { createMachine } from 'xstate';
 
 // Use the machine you created in Exercise 00
 // export const timerMachine = // ...
+
+export const timerMachine = createMachine({
+    initial: 'idle',
+    states: {
+        idle: {
+            on: {
+                TOGGLE: {
+                    target: 'running'
+                }
+            }
+        },
+        running: {
+            on: {
+                TOGGLE: {
+                    target: 'paused'
+                }
+            }
+        },
+        paused: {
+            on: {
+                TOGGLE: {
+                    target: 'running'
+                },
+                RESET: {
+                    target: 'idle'
+                }
+            }
+        },
+    }
+})
